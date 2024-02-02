8c83a51a7dea636e8f31a2fdffea83328aa13d90
Etherfiee <158595560+Etherfiee@users.noreply.github.com>
2024-02-02 23:17:43 +0700

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,22 +1,3 @@
-// trigger to play music in the background with sweetalert
-window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
-});
 
 
 // animation timeline
@@ -27,11 +8,11 @@ const animationTimeline = () => {
 
     textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
         .split("")
-        .join("</span><span>")}</span>`;
+        .join("</span><span>")}</span`;
 
     hbd.innerHTML = `<span>${hbd.innerHTML
         .split("")
-        .join("</span><span>")}</span>`;
+        .join("</span><span>")}</span`;
 
     const ideaTextTrans = {
         opacity: 0,
